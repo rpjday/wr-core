4799a4dc4bb4ffe2a6a3c88ffd5b71b1a8267545
Yunguo Wei <yunguo.wei@windriver.com>
2018-01-26 15:12:27 +0800

    Intel-Gateway: Moving wwan interface to gateway container
    Signed-off-by: Yunguo Wei <yunguo.wei@windriver.com>

--- install_templates/intel-x86-secure/Intel-Gateway-live.sh
+++ install_templates/intel-x86-secure/Intel-Gateway-live.sh
@@ -10,7 +10,7 @@ HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2:console
                       ${ARTIFACTS_DIR}/cube-gw-intel-corei7-64.tar.bz2:console:vty=3,active:net=1:cube.device.mgr=self \
 "
 
-NETWORK_DEVICE="eth+ wl+ en+"
+NETWORK_DEVICE="eth+ wl+ en+ ww+"
 
 #HDINSTALL_CONTAINERS="${ARTIFACTS_DIR}/cube-dom0-intel-corei7-64.tar.bz2"
 INSTALL_ROOTFS="${ARTIFACTS_DIR}/cube-essential-intel-corei7-64.tar.bz2"
